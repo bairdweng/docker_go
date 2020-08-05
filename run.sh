d3478871ffffbee5e7efd166d097efc4929eb5db
bairdweng <>
2020-08-05 14:39:57 +0800

    调整目录

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 # 打包到docker指定的目录
 eval 'cd `dirname $0`'
 eval 'CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./docker/miaoyou_server/app ./main/main.go'
-echo "编译成功"
-eval './docker/miaoyou_server/build.sh'
+echo "编译成功..."
+eval 'cd ./docker/'
+echo "开始执行docker-compose"
+eval 'docker-compose stop && docker rm miaoyou_server_miaoyou_server_1'
+eval 'docker-compose up -d --build'
