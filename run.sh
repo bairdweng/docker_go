3256a002fb365d877205dc614e591c493cffa781
bairdweng <>
2020-08-05 14:47:23 +0800

    最后的更新

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #!/ bin / bash
 # 打包到docker指定的目录
 eval 'cd `dirname $0`'
-eval 'CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./docker/miaoyou_server/app ./main/main.go'
+eval 'CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./docker/miaoyou_server/app/main.go ./main/main.go'
 echo "编译成功..."
 eval 'cd ./docker/'
 echo "开始执行docker-compose"
